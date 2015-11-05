98eee392f0ef918988a6b74e9d8351842fcafbb0
nicolasfujita <nickfujita@gmail.com>
2015-11-05 12:10:19 -0800

    updated mongo server location

--- app/config.js
+++ app/config.js
@@ -1,12 +1,12 @@
 var Promise = require('bluebird');
 var mongoose = Promise.promisifyAll(require('mongoose'));
 
-if(process.env.NODE_ENV === 'production') {
+// if(process.env.NODE_ENV === 'production') {
   //mongoose.connect('mongodb://MongoLab-l:zeJPQzbSoEPpww0FLheAvTPTffOL5aU1625E5bFeq1g-@ds052408.mongolab.com:52408/MongoLab-l');
   mongoose.connect('mongodb://chatterAdmin:chadmin@ds045664.mongolab.com:45664/chatterbox');
-} else {
-  mongoose.connect('mongodb://localhost:27017/shrty');
-}
+// } else {
+//   mongoose.connect('mongodb://localhost:27017/shrty');
+// }
 
 module.exports = mongoose;
 
