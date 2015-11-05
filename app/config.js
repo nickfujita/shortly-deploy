ba792737ff138f3061911693037a9b33a40d49da
nicolasfujita <nickfujita@gmail.com>
2015-11-05 03:16:31 -0800

    changed mongolab url

--- app/config.js
+++ app/config.js
@@ -2,7 +2,8 @@ var Promise = require('bluebird');
 var mongoose = Promise.promisifyAll(require('mongoose'));
 
 if(process.env.NODE_ENV === 'production') {
-  mongoose.connect('mongodb://MongoLab-l:zeJPQzbSoEPpww0FLheAvTPTffOL5aU1625E5bFeq1g-@ds052408.mongolab.com:52408/MongoLab-l');
+  //mongoose.connect('mongodb://MongoLab-l:zeJPQzbSoEPpww0FLheAvTPTffOL5aU1625E5bFeq1g-@ds052408.mongolab.com:52408/MongoLab-l');
+  mongoose.connect('mongodb://chatterAdmin:chadmin@ds045664.mongolab.com:45664/chatterbox');
 } else {
   mongoose.connect('mongodb://localhost:27017/shrty');
 }
