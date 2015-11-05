89b606849ff69e5da37ee1aecf8efe4cf50b8d51
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-04 21:32:19 -0800

    user refactored to mongoose

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,44 @@
-var db = require('../config');
-var crypto = require('crypto');
+var mongoose = require('../config');
+var Schema = mongoose.Schema;
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
-});
+var linkSchema = new Schema({
+  url: {type: String, required: true, unique: true},
+  base_url: {type: String, required: true, unique: true},
+  code: {type: String, required: true, unique: true},
+  title: {type: String, required: true},
+  visits: Number,
+  timestamps: Date
+})
 
+var Link = mongoose.model('Link', linkSchema);
 module.exports = Link;
+
+
+
+// var db = require('../config');
+// var crypto = require('crypto');
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
+
+//       link.string('url', 255);
+//       link.string('base_url', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+
